b2de4ce262a44bee72241ab72313b2833c81b215
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-12 19:10:54 +0600

    Change API endpoint from Mahmud to Abdulla

--- scripts/cmds/bike.js
+++ scripts/cmds/bike.js
@@ -33,7 +33,7 @@ module.exports = {
     
     try {
       const apiUrl = await mahmud();
-      const res = await axios.get(`${apiUrl}/api/album/mahmud/videos/bike?userID=${event.senderID}`);
+      const res = await axios.get(`${apiUrl}/api/album/abdulla/videos/bike?userID=${event.senderID}`);
       if (!res.data.success || !res.data.videos.length)
         return api.sendMessage("❌ | No videos found.", event.threadID, event.messageID);
 
@@ -62,7 +62,7 @@ module.exports = {
       });
     } catch (e) {
       console.error("ERROR:", e);
-      api.sendMessage("🥹error, contact MahMUD.", event.threadID, event.messageID);
+      api.sendMessage("🥹error, contact ÃBDÜLLÄH.", event.threadID, event.messageID);
     }
   }
 };
