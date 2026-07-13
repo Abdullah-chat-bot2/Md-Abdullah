20e26c958818df85a54853e2f0c47c887d5ef0fb
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-13 18:35:16 +0600

    Update author name in comments and error message

--- scripts/cmds/cricket.js
+++ scripts/cmds/cricket.js
@@ -6,7 +6,7 @@ const baseApiUrl = async () => {
 };
 
 /**
-* @author MahMUD
+* @author ÃBDÜLLÄH
 * @author: do not delete it
 */
 
@@ -110,7 +110,7 @@ module.exports = {
       );
     } catch (error) {
       console.error("Error:", error.message);
-      api.sendMessage("🥹error, contact MahMUD.", event.threadID, event.messageID);
+      api.sendMessage("🥹error, contact ÃBDÜLLÄH.", event.threadID, event.messageID);
     }
   }
 };
