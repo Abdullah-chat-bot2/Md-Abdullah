82e30bdfa6a59aa0d72891ba7aa1b1d1277c016c
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-14 09:56:05 +0600

    Update draw.js

--- scripts/cmds/draw.js
+++ scripts/cmds/draw.js
@@ -50,7 +50,7 @@ module.exports = {
 
     } catch (error) {
       console.error(error);
-      api.sendMessage("🥹 Error, contact MahMUD.", event.threadID, event.messageID);
+      api.sendMessage("🥹 Error, contact ÃBDÜLLÄH.", event.threadID, event.messageID);
     }
   }
 };
