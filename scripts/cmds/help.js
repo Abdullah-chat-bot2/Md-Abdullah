996e0c07002a0a6198f5b97a0ec3ef7b3e1ab7ba
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-15 19:32:58 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ ADMIN: 𝑨𝑵𝑫 𝑼𝑳 𝑳𝑨𝑯 \n├‣ WHATSAPP\n╰‣ 01823339492`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
