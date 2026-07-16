80a20158bf2b53eda1df677a5c023e382b2686a5
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-16 14:28:07 +0600

    Change API function from mahmud to abdullah

--- scripts/cmds/memevid.js
+++ scripts/cmds/memevid.js
@@ -27,7 +27,7 @@ module.exports = {
     }
     
     try {
-      const apiUrl = await mahmud();
+      const apiUrl = await abdullah();
       const res = await axios.get(`${apiUrl}/api/album/videos/meme?userID=${event.senderID}`);
       if (!res.data.success || !res.data.videos.length)
         return api.sendMessage("❌ | No videos found.", event.threadID, event.messageID);
@@ -57,7 +57,7 @@ module.exports = {
       });
     } catch (e) {
       console.error("ERROR:", e);
-      api.sendMessage("🥹error, contact MahMUD.", event.threadID, event.messageID);
+      api.sendMessage("🥹error, contact ÃBDÜLLÄH.", event.threadID, event.messageID);
     }
   }
 };
