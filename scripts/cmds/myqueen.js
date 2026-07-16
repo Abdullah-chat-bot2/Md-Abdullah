fb14a998e65471413ff561a2fc005e9a22e64812
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-16 15:27:36 +0600

    Update error message contact to ABDULLAH

--- scripts/cmds/myqueen.js
+++ scripts/cmds/myqueen.js
@@ -72,7 +72,7 @@ module.exports = {
         event.messageID
       );
     } catch (err) {
-      api.sendMessage("🥹error, contact MahMUD." + err.message, event.threadID, event.messageID);
+      api.sendMessage("🥹error, contact ÃBDÜLLÄH." + err.message, event.threadID, event.messageID);
     }
   },
 };
