cb538d535c12838ad86f72b193703ac651296bf5
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-18 12:42:08 +0600

    Update truthordare.js

--- scripts/cmds/truthordare.js
+++ scripts/cmds/truthordare.js
@@ -83,7 +83,7 @@ module.exports = {
       return api.sendMessage("❌ | Invalid command.", event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
-      return api.sendMessage("🥹error, contact MahMUD.", event.threadID, event.messageID);
+      return api.sendMessage("🥹error, contact ÃBDÜLLÄH.", event.threadID, event.messageID);
     }
   }
 };
