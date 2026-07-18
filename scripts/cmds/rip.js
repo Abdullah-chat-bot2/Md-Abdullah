a39b9c3cf9093f5b045e16ac373fc2c47fd1c64d
Abdullah-chat-bot2 <shakil0182333@gmail.com>
2026-07-18 11:14:05 +0600

    Update error messages to reference ÃBDÜLLÄH

--- scripts/cmds/rip.js
+++ scripts/cmds/rip.js
@@ -32,17 +32,17 @@ module.exports = {
                 bn: {
                         noTarget: "× বেবি, কাকে কবর দেবে? মেনশন, রিপ্লাই বা UID দাও! 🐸",
                         success: "𝐄𝐟𝐟𝐞𝐜𝐭 𝐑𝐈𝐏 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐛𝐚𝐛𝐲 <😘",
-                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact MahMUD।"
+                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact ÃBDÜLLÄH।"
                 },
                 en: {
                         noTarget: "× Baby, mention, reply, or provide UID of the target! 🐸",
                         success: "𝐄𝐟𝐟𝐞𝐜𝐭 𝐑𝐈𝐏 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐛𝐚𝐛𝐲 <😘",
-                        error: "× API error: %1. Contact MahMUD for help."
+                        error: "× API error: %1. Contact ÃBDÜLLÄH for help."
                 },
                 vi: {
                         noTarget: "× Cưng ơi, hãy đề cập, phản hồi hoặc cung cấp UID! 🐸",
                         success: "𝐄𝐟𝐟𝐞𝐜𝐭 𝐑𝐈𝐏 𝐭𝐡𝐚̀𝐧𝐡 𝐜𝐨̂𝐧𝐠 <😘",
-                        error: "× Lỗi: %1. Liên hệ MahMUD để hỗ trợ."
+                        error: "× Lỗi: %1. Liên hệ ÃBDÜLLÄH để hỗ trợ."
                 }
         },
 
